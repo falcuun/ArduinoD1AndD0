42c747a1933085b128f5dbf7ec11132c304c60d1
falcuun <falcuun@gmail.com>
2019-01-29 22:50:50 +0100

    Added very basic LED blinking on RX pin

---
+++ ArdD0&D1/ArdD0_D1/ArdD0_D1.ino
@@ -0,0 +1,26 @@
+const int LED_PIN0 = 0;
+const int LED_PIN1 = 1;
+
+void setup() {
+  pinMode(LED_PIN0, OUTPUT);
+}
+
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    digitalWrite(LED_PIN0, ledState);
+  }
+}
