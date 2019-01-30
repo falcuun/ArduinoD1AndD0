249cb824978cf9f0147234ef2344b59b76c3541e
falcuun <falcuun@gmail.com>
2019-01-30 20:02:03 +0100

    Software modded to also display an approach at 'Threading' with the arduino

--- ArdD0&D1/ArdD0_D1/ArdD0_D1.ino
+++ ArdD0&D1/ArdD0_D1/ArdD0_D1.ino
@@ -7,11 +7,28 @@ void setup() {
 }
 
 int ledState = LOW;
+int longer_led_state = LOW;
+
 unsigned long previousMillis = 0;
+unsigned long longer_previousMillis = 0;
+
 const long interval = 1000;
+const long longer_interval = 5000;
 
 void loop() {
   unsigned long currentMillis = millis();
+
+  if (currentMillis - longer_previousMillis >= longer_interval)
+  {
+    longer_previousMillis = currentMillis;
+
+    if (longer_led_state == LOW) {
+      longer_led_state = HIGH;
+    } else {
+      longer_led_state = LOW;
+    }
+    digitalWrite(LED_PIN1, longer_led_state);
+  }
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
@@ -21,8 +38,9 @@ void loop() {
     } else {
       ledState = LOW;
     }
-
     digitalWrite(LED_PIN0, ledState);
-    digitalWrite(LED_PIN1, !ledState);
   }
+
+
+
 }
